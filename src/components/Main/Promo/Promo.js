7169665749d820518eed9aa04bde1d722413fdcc
Artem Tkachev <79772674001@ya.ru>
2021-09-20 14:41:24 +0300

    Верстка промо

---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Promo.css';
+
+function Promo() {
+  return (
+    <>
+      <section className='promo'>
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      </section>
+      <nav className="promo__nav">
+        <a href="#" className="link">О проекте</a>
+        <a href="#" className="link">Технологии</a>
+        <a href="#" className="link">Студент</a>
+      </nav>
+    </>
+  );
+}
+
+export default Promo;
