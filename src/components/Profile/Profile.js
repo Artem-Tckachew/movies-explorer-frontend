7ccf1b23c79646c71748c5a44f86ff7939a8a1d1
Artem-Tckachew <79772674001@ya.ru>
2021-10-19 11:38:53 +0300

    поправил ошибки обновления профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -18,7 +18,7 @@ function Profile({ handleSignOut, handleUpdateUser, isSuccess, isError, setSucce
     setHasChanges(
       !(values.name === name) || (!(values.email === email) && isValid)
     );
-  }, [values.name, values.email, name, email, isValid]);
+  }, [values.name, values.email, name, email]);
 
   const onEditSubmit = (evt) => {
     evt.preventDefault();
@@ -33,7 +33,7 @@ function Profile({ handleSignOut, handleUpdateUser, isSuccess, isError, setSucce
     <div className="profile">
       <Header isAuth={true} />
       <div className="profile__container">
-        <h2 className="profile__title">Привет, {values.name}!</h2>
+        <h2 className="profile__title">Привет, {name}!</h2>
         <form className="profile__form" onSubmit={onEditSubmit}>
           <label className="profile__input-container">
             <span className="profile__input-title">Имя</span>
